7798db2920b493f459a737ab45a52fa79c7ed5d2
Michael Fudge <mafudge@syr.edu>
2014-05-08 12:11:51 -0400

    Basic Shell with Forms-Based AD Authentication

---
+++ src/RemoteLab/RemoteLab.Authentication/ActiveDirectoryAuthentication.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+
+namespace RemoteLab.Authentication  
+{
+    /// <summary>
+    /// Active Directory Authentication
+    /// </summary>
+    public class ActiveDirectoryAuthentication : IAuthentication 
+    {
+        public string Domain { get; set; }
+        private string Path;
+        public ActiveDirectoryAuthentication(string domain) 
+        {
+            this.Domain = domain;
+            this.Path = "LDAP://" + domain;
+        }
+
+        public bool Authenticate(string UserName, string Password) 
+        {
+            bool authenticated = false;
+            try
+            {
+                DirectoryEntry entry = new DirectoryEntry(this.Path,UserName,Password);
+                object result = entry.NativeObject;
+                authenticated = true;
+            }
+            catch (DirectoryServicesCOMException e) 
+            { 
+                // authentication fail 
+            }
+            return authenticated;
+        }
+
+    }
+}
---
+++ src/RemoteLab/RemoteLab.Authentication/IAuthentication.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace RemoteLab.Authentication
+{
+    /// <summary>
+    /// Implement this interface if you desire a different method of forms authentication such as LDAP
+    /// </summary>
+    public interface IAuthentication
+    {
+        bool Authenticate(string UserName, string Password);
+    }
+}
